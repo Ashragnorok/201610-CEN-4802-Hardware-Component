b6c0907d9ba6d5cad7270ba05c5c14f6cdf73f9a
Ashragnorok <Ashneotheone@gmail.com>
2016-02-29 18:42:38 -0500

    Initial Solution Commit

---
+++ Sark - Hardware Component/Sark - Hardware Component/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sark___Hardware_Component
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
